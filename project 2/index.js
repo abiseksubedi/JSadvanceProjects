0a521b7a26e03d11e5cd0393c72178409774ab1b
Abisek <abiseksubedi8@gmail.com>
2021-08-16 13:24:50 +0545

    added  async await methods

--- project 2/index.js
+++ project 2/index.js
@@ -3,36 +3,37 @@ const jokes = document.querySelector('#jokes')
 
 // promise way of fetching datas from API !
 
-function renderJokes() {
+// function renderJokes() {
 
-    const setHeader = {
-        headers: {
-            Accept: "application/json"
-        }
-    }
-    fetch('https://icanhazdadjoke.com', setHeader)
-        .then((respose) => respose.json())
-        .then((data) => {
-            jokes.innerHTML = data.joke
-        }).catch((error) => console.log(error))
-}
-
-
-
-// fetching data using Async await method from API 
-
-// const renderJokes = async() => {
-//     try {
-//         const setHeader: {
-//             headers: {
-//                 Accept: 'application/json'
-//             }
+//     const setHeader = {
+//         headers: {
+//             Accept: "application/json"
 //         }
-//         const req = await fetch('https://icanhazdadjoke.com', setHeader)
-//         const data = await req.json();
-//         jokes.innerHTML = data.joke;
-//     } catch (err) { console.log(`the error is ${err}`) }
+//     }
+//     fetch('https://icanhazdadjoke.com', setHeader)
+//         .then((respose) => respose.json())
+//         .then((data) => {
+//             jokes.innerHTML = data.joke
+//         }).catch((error) => console.log(error))
 // }
+
+
+
+// fetching data using Async await method from API
+
+const renderJokes = async() => {
+    try {
+        const setHeader = {
+            headers: {
+                Accept: "application/json"
+            }
+        }
+
+        req = await fetch('https://icanhazdadjoke.com', setHeader)
+        const data = await req.json();
+        jokes.innerHTML = data.joke;
+    } catch (err) { console.log(`the error is ${err}`) }
+}
 
 next.addEventListener("click", function() {
     renderJokes();
