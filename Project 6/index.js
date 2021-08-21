1a5e37c78742fbcb4799a2fbe17e5a52fb8f39b0
Abisek <abiseksubedi8@gmail.com>
2021-08-21 22:37:47 +0545

    timer function added !

--- Project 6/index.js
+++ Project 6/index.js
@@ -1,17 +1,21 @@
-// 
-// 
-
 const counters = document.querySelectorAll('.counter');
-
-
-
-
 counters.forEach((counter) => {
     counter.innerHTML = 0;
 
-    updateCounter = () => {
-        targetCount = counter.getAttribute('data-target');
-        console.log(targetCount);
+    const updateCounter = () => {
+        const targetCount = +counter.getAttribute('data-target');
+
+        const startingCount = Number(counter.innerHTML);
+        const incr = targetCount / 100;
+
+        if (startingCount < targetCount) {
+            counter.innerHTML = `${Math.round(startingCount + incr)}`
+            setTimeout(updateCounter, 10);
+        } else {
+            counter.innerHTML = targetCount;
+        }
+
+
     }
-    updateCounter()
+    updateCounter();
 })
